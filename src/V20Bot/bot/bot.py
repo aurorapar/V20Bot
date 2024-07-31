4e2ec37d1a1bab48b03a3ada87385fec8456a040
aurorpar <aurorapariseau@gmail.com>
2024-07-31 01:38:06 -0500

    added emojies as roll results;syncing system;exception tidying;permission corrects;bug fixes

--- src/V20Bot/bot/bot.py
+++ src/V20Bot/bot/bot.py
@@ -1,12 +1,11 @@
 import sys
-import traceback
 from typing import Optional, Any
 
 import discord
 from discord import app_commands
 
 from .event_handlers import message, commands
-from ..settings.settings import BOT_USERNAME
+from ..settings.settings import BOT_USERNAME, TESTING, RESYNC
 
 
 class DiscordBot(discord.Client):
@@ -14,25 +13,37 @@ class DiscordBot(discord.Client):
     def __init__(self, *, intents: discord.Intents):
         super().__init__(intents=intents)
         self.tree = app_commands.CommandTree(self)
+        self.synced = False
 
-    def on_error(self, event_method: str, /, *args: Any, **kwargs: Any) -> None:
+    async def on_error(self, event_method: str, /, *args: Any, **kwargs: Any) -> None:
         error = sys.exc_info()
         error_message = error[1]
         trace = error[2]
-        print(event_method)
         print(error_message)
-        print(traceback.print_tb(trace))
 
     async def on_message(self, discord_message):
-        print("Syncing tree")
-        synced = await self.tree.sync()
-        print(f"Tree synced!")
-        print(synced)
-        for x in dir(synced[0]):
-            print(f"{x}={getattr(synced[0], x)}")
+        if discord_message.content.lower() == 'resync':
+            if len([role for role in discord_message.author.roles if role.permissions.administrator]):
+                await discord_message.channel.delete_messages(messages=[discord_message])
+                if not RESYNC:
+                    await discord_message.author.send(content='Resyncing has been disabled.')
+                    return
+                if self.synced:
+                    await discord_message.author.send(content='The bot has already synced.')
+                    return
+                await discord_message.author.send(content='Resyncing....')
+                await self.tree.sync()
+                await discord_message.author.send(content='Resynced!')
+                self.synced = True
+        if not self.synced and RESYNC:
+            await self.tree.sync()
+            print(f"Tree synced!")
+            self.synced = True
 
     async def on_ready(self):
         await self.user.edit(username=BOT_USERNAME)
+        if TESTING:
+            return
         for guild in self.guilds:
             member = await self.get_self_member(guild)
             await message.send_message(member, f'{self.user.name} has come out of torpor.')
@@ -49,6 +60,7 @@ intents.messages = True
 intents.guilds = True
 intents.presences = True
 intents.members = True
+intents.message_content = True
 discord_bot = DiscordBot(intents=intents)
 
 
@@ -59,11 +71,27 @@ discord_bot = DiscordBot(intents=intents)
     dicepool='The total number of dice you have in your dice pool, including auto-successes',
     autosuccesses='The number of auto-successes (due to Disciplines)',
     specialized='Whether you are specialized in the roll',
-    willpowerused='Whether Willpower was spent on the roll'
+    willpowerused='Whether Willpower was spent on the roll',
+    target='A potential player you wish to have a contested roll with'
 )
-async def roll(interaction: discord.Interaction, difficulty: int, dicepool: int, autosuccesses: Optional[int] = 0,
-              specialized: Optional[bool] = False, willpowerused: Optional[bool] = False):
-    return await commands.handle_roll(interaction, difficulty, dicepool, autosuccesses, specialized, willpowerused)
+async def roll(
+        interaction: discord.Interaction,
+        difficulty: int,
+        dicepool: int,
+        autosuccesses: Optional[int] = 0,
+        specialized: Optional[bool] = False,
+        willpowerused: Optional[bool] = False,
+        target: Optional[discord.Member] = None):
+    return await commands.handle_roll(
+        client=discord_bot,
+        interaction=interaction,
+        difficulty=difficulty,
+        dice_pool=dicepool,
+        auto_successes=autosuccesses,
+        specialized=specialized,
+        target=target,
+        willpower_used=willpowerused
+    )
 
 
 @discord_bot.tree.command()
@@ -74,5 +102,9 @@ async def roll(interaction: discord.Interaction, difficulty: int, dicepool: int,
 )
 async def setbotchannel(interaction: discord.Interaction, channel: discord.TextChannel):
     member = await discord_bot.get_self_member(interaction.guild)
-    await commands.set_bot_channel(member, interaction, channel)
+    await commands.set_bot_channel(
+        member,
+        interaction,
+        channel
+    )
 
--- src/V20Bot/settings/settings.py
+++ src/V20Bot/settings/settings.py
@@ -2,4 +2,5 @@
 BOT_TOKEN = None
 # BOT_TOKEN = "EXAMPLE_TOKEN"
 BOT_USERNAME = "VTM Bot"
-
+TESTING = False
+RESYNC = False
